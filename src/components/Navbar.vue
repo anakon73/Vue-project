1ae0397b3ef0ce7c20ca41e43195c276122e2dc4
anakon73 <nazarkinah73@gmail.com>
2022-08-22 19:32:34 +0300

    feat: added routerC

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Vue 3</div>
+    <div class="navbar__btns">
+      <MyButton @click="$router.push('/posts')">Посты</MyButton>
+      <MyButton @click="$router.push('/about')" style="margin-left: 20px"
+        >О сайте</MyButton
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "./UI/MyButton.vue";
+export default { components: { MyButton } };
+</script>
+
+<style>
+.navbar {
+  height: 50px;
+  background-color: lightgray;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainVue from "../pages/Main.vue";
+import PostPageVue from "../pages/PostPage.vue";
+import AboutVue from "../pages/About.vue";
+import PostIdPageVue from "../pages/PostIdPage.vue";
+
 const routes = [
   {
     path: "/",
     component: MainVue,
+  },
+  {
+    path: "/posts",
+    component: PostPageVue,
+  },
+  {
+    path: "/about",
+    component: AboutVue,
+  },
+  {
+    path: "/posts/:id",
+    component: PostIdPageVue,
   },
 ];
 const router = createRouter({
